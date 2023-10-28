a182a223394fde2dc0d169f7f8394899eb00f8d4
TheAlphaApp <souravsuman636@gmail.com>
2023-10-28 18:28:47 +0530

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ display: 'swap', subsets: ['latin'] });
+
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
